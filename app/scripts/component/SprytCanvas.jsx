b0f49fe464638b2d924cd752369b508ff26078af
brookjordan <brkjrdn@gmail.com>
2016-10-05 22:23:01 +0800

    Get stuff working

--- app/scripts/component/SprytCanvas.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class SprytCanvas extends React.Component {
-  render() {
-    return <canvas id="spryt-canvas"></canvas>
-  }
-}
--- app/scripts/entry/main.js
+++ app/scripts/entry/main.js
@@ -1,27 +1,57 @@
-import React          from 'react';
-import ReactDOM       from 'react-dom';
-import buildWorld     from '../method/buildWorld.js';
-import SprytCanvas    from '../component/SprytCanvas.jsx'
+import renderCanvas   from '../method/canvasSetter.js'
 
-ReactDOM.render(<SprytCanvas/>, document.getElementById('app-entry'));
 
-const WORLD = buildWorld({
-  zoom:   87/Math.PI,
-  canvas: document.querySelectorAll('p')[10],
+const canvasElement = document.getElementById('spryt-board');
+const boardWidth    = 9;
+const boardHeight   = 12;
+const retinaScale   = Window.devicePixelRatio;
+
+const sprytCanvas = renderCanvas(canvasElement, boardWidth, boardHeight, retinaScale);
+const { resize, canvas, ctx, x, y, s, sq } = sprytCanvas;
+const drawSquare = squareDropper({ before: showBounds });
+
+canvasElement.style.transform = `scale(${1 / retinaScale})`;
+
+resize();
+showBounds();
+
+window.addEventListener('resize', () => {
+  resize();
+  drawSquare();
 });
 
-const coords = {
-  type: 'world',
-  x:    10,
-  y:    10,
-};
 
-const canvasCoords = WORLD.coords2canvas(coords);
+function showBounds() {
+  ctx.fillStyle = 'rgba(0,255,0,1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'rgba(0,0,255,1)';
+  ctx.fillRect(...sq(0, 0, boardWidth, boardHeight));
+}
 
-const hello = () => { console.log('hello'); return false; }
+function squareDropper({before=() => {}, after=() => {}} = {}) {
+  const squareSize = 3;
+  let x = (boardWidth / 2) - (squareSize / 2);
+  let y = -squareSize;
 
-hello();
+  dropTheSquare();
 
-console.log(coords);
-console.log(WORLD.coords2canvas(coords));
-console.log(WORLD.coords2world(canvasCoords));
+  return draw;
+
+  function dropTheSquare() {
+    y += 0.05;
+    x += Math.random() * 0.2 - 0.1;
+    if (y > boardHeight) {
+      y = -squareSize;
+      x = (boardWidth / 2) - (squareSize / 2);
+    }
+    draw();
+    requestAnimationFrame(dropTheSquare);
+  }
+
+  function draw() {
+    before();
+    ctx.fillStyle = 'rgb(255,0,0)';
+    ctx.fillRect(...sq(x, y, squareSize, squareSize));
+    after();
+  }
+}
